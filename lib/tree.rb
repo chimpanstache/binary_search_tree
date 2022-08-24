55ea1cfd4d60d6dfb8a55256d23a165a464aad15
chimpanstache <53328741+chimpanstache@users.noreply.github.com>
2022-08-24 22:19:05 +0200

    insert algo

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,6 +4,7 @@ class Tree
 
   def initialize(array)
     @array = array
+    @root = build_tree
   end
 
   def sort(left, right)
@@ -47,12 +48,31 @@ class Tree
     building_tree(merge_sort(@array.uniq), 0, (@array.uniq.size - 1))
   end
 
-  def pretty_print(node = build_tree, prefix = '', is_left = true)
+  def insert(value, node = @root)
+    return nil if @array.include?(value)
+
+    if value < node.data
+      return node.left = Node.new(value) if node.left.nil?
+      
+      insert(value, node.left)
+    else
+      return node.right = Node.new(value) if node.right.nil?
+
+      insert(value, node.right)
+    end
+  end
+
+  def delete
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
-tree = Tree.new([6, 4, 7, 5, 3, 2, 9, 11, 8, -1, 0, 1, 2, -2, -3, -4])
+tree = Tree.new([4, 5, 2, 11, -1, 1, -2, -4])
+# tree.pretty_print
+tree.insert(3)
 tree.pretty_print
